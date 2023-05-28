30d4bebdf1ddb7ff6d4f9537f896fa37653fbecf
muhdjabir <e0725438@u.nus.edu>
2023-05-28 14:38:04 +0800

    Created models for Event Task Team and User

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -7,12 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 POSTGRES_URI =  os.getenv("POSTGRES_URI")
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRES_URI
-# app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
-# app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=3)
 db = SQLAlchemy(app)
 CORS(app)
 
@@ -21,6 +18,8 @@ CORS(app)
 
 # app.register_blueprint(auth)
 # app.register_blueprint(routes)
+
+from .models import User, Task, Event, Team
 
 with app.app_context():
     db.create_all()
---
+++ server/src/models.py
@@ -0,0 +1,91 @@
+from flask_sqlalchemy import SQLAlchemy
+# from werkzeug.security import generate_password_hash, check_password_hash
+from src import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(), nullable = False)
+    email = db.Column(db.String(), nullable = False, unique = True)
+    role = db.Column(db.String(), nullable = False)
+
+    def __repr__(self):
+        return f'User: {self.id} {self.name} {self.role}'
+    
+    def __init__(self, id, name, email, role):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.role = role
+
+def format_user(user):
+    return {
+        "id": user.id,
+        "email": user.email,
+        "username": user.username
+    }
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(), nullable = False, default = "Open")
+    status = db.Column(db.String(), nullable = False)
+
+    def __repr__(self):
+        return f'Task ID: {self.id} Description: {self.description} Status: {self.status}'
+    
+    def __init__(self, description):
+        self.description = description
+
+def format_task(task):
+    return { 
+        "id": task.id,
+        "description": task.description,
+        "status": task.status
+    }
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(), nullable = False)
+    description = db.Column(db.String(), nullable = False)
+    manager_id = db.Column(db.Integer, nullable = False)
+    members_id = db.Column(db.ARRAY(db.Integer), server_default= "{}", nullable = False)
+    events_id = db.Column(db.ARRAY(db.Integer), server_default= "{}")
+
+    def __repr__(self):
+        return f'ID: {self.id} Name: {self.name} Description: {self.description}'
+
+    def __init__(self, name, description, manager_id):
+        self.name = name
+        self.description = description
+        self.manager_id = manager_id
+
+def format_team(team):
+    return {
+        "id": team.id,
+        "name": team.name,
+        "description": team.description,
+        "manager_id": team.manager_id,
+        "members_id": team.members_id,
+        "events_id": team.events_id
+    }
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(), nullable = False)
+    time = db.Column(db.DateTime, nullable = False)
+    team_id = db.Column(db.Integer, nullable = False)
+
+    def __repr__(self):
+        return f'ID: {self.id} Description: {self.description} Time: {self.time} Team: {self.team_id}'
+    
+    def __init__(self, description, time, team_id):
+        self.description = description
+        self.time = time
+        self.team_id = team_id
+
+def format_event(event):
+    return {
+        "id": event.id,
+        "description": event.description,
+        "time": event.team,
+        "team_id": event.team_id
+    }
