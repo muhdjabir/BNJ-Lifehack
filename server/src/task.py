60aefb6e80eb0f1150338fc347b407eba58925e8
muhdjabir <e0725438@u.nus.edu>
2023-05-28 16:06:39 +0800

    Added get tasks based on array

--- server/src/task.py
+++ server/src/task.py
@@ -15,7 +15,7 @@ def create_task():
     task = Task(description, priority)
     db.session.add(task)
     db.session.commit()
-    return format_task(task)
+    return format_task(task), 200
 
 @tasks.route("/api/task", methods = ["GET"])
 def get_tasks():
@@ -23,20 +23,20 @@ def get_tasks():
     task_list = []
     for task in tasks:
         task_list.append(format_task(task))
-    return {'tasks': task_list}
+    return {'tasks': task_list}, 200
 
 @tasks.route("/api/task/<id>", methods = ["GET"])
 def get_task(id):
     task = Task.query.filter_by(id = id).one()
     formatted_task = format_task(task)
-    return { 'task': formatted_task}
+    return { 'task': formatted_task}, 200
 
 @tasks.route("/api/task/<id>", methods = ["DELETE"])
 def delete_task(id):
     task = Task.query.filter_by(id = id).one()
     db.session.delete(task)
     db.session.commit()
-    return f'Task (id: {task.id}, Item: {task.description})'
+    return f'Deleted Task (id: {task.id}, Item: {task.description})', 200
 
 @tasks.route("/api/task/<id>", methods = ["PATCH"])
 def update_event(id):
@@ -44,4 +44,19 @@ def update_event(id):
     status = request.json['status']
     task.update(dict(status = status))
     db.session.commit()
-    return {'task': format_task(task.one())}
+    return {'task': format_task(task.one())}, 200
+
+@tasks.route("/api/task/item", methods = ["GET"])
+def get_my_task():
+    task_id = request.json["task"]
+    return {"tasks": task_id}
+
+@tasks.route("/api/task/items", methods = ["GET"])
+def get_team_tasks():
+    task_id = request.json["task"]
+    print(task_id)
+    tasks = Task.query.filter(Task.id.in_(task_id)).all()
+    task_list = []
+    for task in tasks:
+        task_list.append(format_task(task))
+    return {'tasks': task_list}, 200
